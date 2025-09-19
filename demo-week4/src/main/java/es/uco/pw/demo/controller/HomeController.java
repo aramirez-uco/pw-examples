8fbfc680ffc77ef09c6864e0123cea1730491013
Aurora Ramírez <aurorarq89@gmail.com>
2025-09-19 12:45:40 +0200

    Remove unused repository from home controller

--- demo-week4/src/main/java/es/uco/pw/demo/controller/HomeController.java
+++ demo-week4/src/main/java/es/uco/pw/demo/controller/HomeController.java
@@ -3,22 +3,11 @@ package es.uco.pw.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import es.uco.pw.demo.model.repository.StudentRepository;
-
 @Controller
 public class HomeController {
 
-    StudentRepository studentRepository;
-        
-    public HomeController(StudentRepository studentRepository){
-        this.studentRepository = studentRepository;
-        String sqlQueriesFileName = "./src/main/resources/db/sql.properties";
-        this.studentRepository.setSQLQueriesFileName(sqlQueriesFileName);
-    }
-
     @GetMapping("/")
     public String home() {
-
         return new String("home");
     }
     
